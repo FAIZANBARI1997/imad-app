7839555164f68ddca803485e5ee097803d4a49cb
FAIZANBARI1997 <30802604+FAIZANBARI1997@users.noreply.github.com>
2017-08-10 19:10:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,16 @@
 var button=document.getElementById('counter');
 var counter=0;
 
-button.onlick=
+button.onclick=function(){
+    //make a request to counter
+    
+    //capture response in a variable
+    
+    //render in correct span
+    
+    counter+counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
 
 
