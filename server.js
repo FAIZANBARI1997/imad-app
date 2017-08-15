a4cc7cbdad37894491edd58eb0d507ca8794e4f3
FAIZANBARI1997 <30802604+FAIZANBARI1997@users.noreply.github.com>
2017-08-15 18:09:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.get('/articles/:articleName', function (req, res) {
     
   var articleName=req.params.articleName;    
   
-  pool.query("SELECT * FROM sql1 WHERE title = '" + req.params.articleName + "'", function (err, result){
+  pool.query("SELECT * FROM sql1 WHERE title = $1",[req.params.articleName], function (err, result){
         if (err)
         {
            res.status(500).send(err.toString()); 
